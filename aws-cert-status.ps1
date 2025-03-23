60fb1c2a7ca3bbe2f77ef30a636acacb4ca49689
rob-git-user <53093761+rob-git-user@users.noreply.github.com>
2025-03-23 18:03:51 +0100

    first commit

---
+++ aws-cert-status.ps1
@@ -0,0 +1,25 @@
+# aws-cert-status.ps1
+# ARN is in file cert-arn.txt
+# Usage: powershell -File aws-cert-status.ps1
+$certArn = Get-Content -Path "./cert-arn.txt"
+
+Write-Host "📡 ACM cert monitoring has started..."
+Write-Host "----------------------------"
+
+do {
+    $status = aws acm describe-certificate `
+        --region us-east-1 `
+        --certificate-arn $certArn `
+        --query "Certificate.Status" `
+        --output text
+
+    $now = Get-Date -Format "HH:mm:ss"
+    Write-Host "$now → Státusz: $status"
+
+    if ($status -ne "ISSUED") {
+        Start-Sleep -Seconds 120
+    }
+
+} while ($status -ne "ISSUED")
+
+Write-Host "✅ Certificate is ISSUED -> Terraform apply! 🚀"
